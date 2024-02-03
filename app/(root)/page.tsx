eb161d4b46b16d7c999a52d05130e9bab530ef72
IanNien <ianexcellent@gmail.com>
2024-02-04 03:19:04 +0800

    modal components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,28 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+"use client"
 
-export default function Home() {
+import { Button } from "@/components/ui/button"
+import { Modal} from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { UserButton } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
+
+const  SetupPage = () => {
+    const onOpen = useStoreModal((state) => state.onOpen)
+    const isOpen = useStoreModal((state) => state.isOpen)
+
+    useEffect(() => {
+        if(!isOpen){
+            onOpen() ;
+        }
+    }, [isOpen, onOpen])
+
   return(
     
     <div className="p-4">
-        <Button>Click Me</Button>
-        <UserButton afterSignOutUrl="/"/>
-    </div>
+        Root Page
+    </div>  
   )
 }
+
+export default SetupPage;
  
